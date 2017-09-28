c77872415056789fd6e7a2b37339dd15fe0315bf
Kseniya Ivanova-Baker <kivanova.baker@gmail.com>
2017-09-28 18:39:43 -0500

    Working index.html page, no error field yet

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, request, redirect, render_template
+import cgi
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/")
+def index():
+    return render_template('index.html', title="User-Signup")
+
+app.run()

59857667a3e7bbec8484d534c596bb583d7ebf0c
Kseniya Ivanova-Baker <kivanova.baker@gmail.com>
2017-09-29 13:04:55 -0500

    Made email field optional and added css sheets

--- main.py
+++ main.py
@@ -9,13 +9,7 @@ def index():
     return render_template('index.html', title="User-Signup")
 
 @app.route("/", methods=['POST'])
-#where errors will be displayed:
-#error if required field empty
-#         user name or password contains a space, <3 or >20 chars
-#         two passwords don't match
-#         email invalid (does not contain one '@' and '.', contains space, <3 or >20 chars )
-#error displayed near related field
-#for username and email type, preserve if no error
+
 def validate_input():
     username = request.form['username']
     password = request.form['password']
@@ -28,15 +22,18 @@ def validate_input():
     email_error=""
 
     if " " in username or len(username)<3 or len(username)>20:
-        username_error = "Please enter a valid username"
+        username_error = "Please enter a valid username (3-20 chars, no spaces)"
         username = ""
     if " " in password or len(password)<3 or len(password)>20:
-        password_rule_error = "Please enter a valid password"
+        password_rule_error = "Please enter a valid password (3-20 chars, no spaces)"
     if password != v_password:
         match_error = "Please enter matching passwords"
-    if email.count("@") != 1 or email.count(".") != 1 or " " in email or len(email)<3 or len(email)>20:
-        email_error = "Please enter a valid email"
-        email = ""
+    if len(email) == 0:
+        email_error = ""
+    else:
+        if email.count("@") != 1 or email.count(".") != 1 or " " in email or len(email)<3 or len(email)>20:
+            email_error = "Please enter a valid email"
+            email = ""
 
     if not username_error and not password_rule_error and not match_error and not email_error:
         return render_template('welcome.html', title='Welcome!', username=username)
